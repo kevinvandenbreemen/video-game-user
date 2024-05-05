f5aa6fbc7f9ac980483cf0ff7c43b4f337ee32f8
Kevin <kevin.vandenbreemen@gmail.com>
2024-05-04 18:07:33 -0700

    Basic first bit of code to demonstrate fetching package published to github packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+
+    //  Attempting to use the package, based on instructions at
+    //  https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-gradle-registry#using-a-published-package
     repositories {
         maven {
-            url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
+            url = uri("https://maven.pkg.github.com/kevinvandenbreemen/video-display-raster")
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
                 password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
@@ -21,7 +24,8 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
 
-    implementation("com.vandenbreemen:video-display-raster:1.0.0")
+    val videoDisplayRasterVersion = "1.0.1"
+    implementation("com.vandenbreemen:video-display-raster:$videoDisplayRasterVersion")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,11 @@
 package com.vandenbreemen
 
+import com.vandenbreemen.viddisplayrast.data.GameDataRequirements
+
 fun main() {
     println("Hello World!")
+
+    val requirement =
+        GameDataRequirements(100, 200, 8, 8, 1024)
+    println(requirement)
 }
