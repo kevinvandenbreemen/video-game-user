3cf5deebca731d5ac4ff55e9b689b5a2d59c732c
Kevin <kevin.vandenbreemen@gmail.com>
2024-05-04 21:12:34 -0700

    Display data from the raster

--- src/main/kotlin/com/vandenbreemen/videogameusr/view/Composables.kt
+++ src/main/kotlin/com/vandenbreemen/videogameusr/view/Composables.kt
@@ -1,27 +1,50 @@
 package com.vandenbreemen.com.vandenbreemen.videogameusr.view
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import com.vandenbreemen.viddisplayrast.data.DisplayRaster
 
 @Composable
-fun TestDisplay() {
+fun RasterDisplay(raster: DisplayRaster) {
 
-    Column(modifier = Modifier.fillMaxSize().background(androidx.compose.ui.graphics.Color.Gray)) {
-        Column {
-            Text("Hello from Compose Bitch")
+
+    Column {
+
+        //  Render a white square
+        Canvas(modifier=Modifier.fillMaxSize()) {
+
+            //  Display all the pixels:
+            val pixelWidthInCanvas = size.width / raster.xDim
+            val pixelHeightInCanvas = size.height / raster.yDim
+
+            for (y in 0 until raster.yDim) {
+                for (x in 0 until raster.xDim) {
+                    val left = x * pixelWidthInCanvas
+                    val top = y * pixelHeightInCanvas
+                    val color = if (raster.getPixel(
+                            x,
+                            y
+                        ) > 0
+                    ) androidx.compose.ui.graphics.Color.White else androidx.compose.ui.graphics.Color.Black
+                    drawRect(color, topLeft = Offset(left, top), size = Size(pixelWidthInCanvas, pixelHeightInCanvas))
+                }
+            }
         }
-    }
 
+    }
 
 }
 
 @Composable
 @Preview
-fun PreviewTestComposable() {
-    TestDisplay()
+fun PreviewRasterDisplay() {
+    val raster = DisplayRaster(16, 16)
+    raster.setPixel(8, 8, 1)
+    RasterDisplay(raster)
 }
