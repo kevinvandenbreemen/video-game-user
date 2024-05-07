fe69df9a1964e982cd4893d8e54a696ae2b8a9e4
Kevin <kevin.vandenbreemen@gmail.com>
2024-05-06 18:25:44 -0700

    Introduce level of difficulty

--- src/main/kotlin/com/vandenbreemen/videogameusr/game/vidgame1/VideoGame1Model.kt
+++ src/main/kotlin/com/vandenbreemen/videogameusr/game/vidgame1/VideoGame1Model.kt
@@ -9,6 +9,11 @@ class VideoGame1Model(screenWidth: Int, screenHeight: Int) {
 
     private val motionIncrement = 1
 
+    private val enemyTurnDelayFractionOfSecond = 2  //  As this increases it makes the game harder!
+    private val enemyTurnDelay: Long = 1000 / enemyTurnDelayFractionOfSecond.toLong()
+    private var lastTurnTime: Long = System.currentTimeMillis()
+
+
     private var playerLocation: Pair<Int, Int> = Pair((screenWidth * 0.1).toInt(), screenHeight / 2)
     private var enemyLocation: Pair<Int, Int> = Pair((screenWidth * 0.9).toInt(), screenHeight / 2)
 
@@ -46,11 +51,19 @@ class VideoGame1Model(screenWidth: Int, screenHeight: Int) {
      */
     fun playGamesTurn() {
 
+        val currentTime = System.currentTimeMillis()
+        if(currentTime - lastTurnTime < enemyTurnDelay) {
+            return
+        }
+
         //  Move the enemy toward the player
         enemyLocation = Pair(
             if (enemyLocation.first > playerLocation.first) enemyLocation.first - motionIncrement else enemyLocation.first + motionIncrement,
             if (enemyLocation.second > playerLocation.second) enemyLocation.second - motionIncrement else enemyLocation.second + motionIncrement
         )
+        lastTurnTime = currentTime
+
+        //  TODO    Figure out when we want to increase the enemy speed!
 
     }
 
