b613be81014ec4b2024e1ad7007a040b4fa2c79f
Kevin <kevin.vandenbreemen@gmail.com>
2024-05-08 09:43:51 -0700

    Game 2 - movement

---
+++ src/main/kotlin/com/vandenbreemen/videogameusr/game/videogame2/VideoGame2Model.kt
@@ -0,0 +1,33 @@
+package com.vandenbreemen.com.vandenbreemen.videogameusr.game.videogame2
+
+import com.vandenbreemen.com.vandenbreemen.videogameusr.model.ScreenInteractor
+
+class VideoGame2Model(
+    private val screenWidth: Int, private val screenHeight: Int, private val spriteWidth: Int, private val spriteHeight: Int,
+) {
+
+    private var playerGoingRight = false
+    private var movementIncrement = 1   //  TODO    Consider accelerating the player
+    private var playerLocation = Pair((screenWidth *0.5).toInt(), (screenHeight * 0.5).toInt())
+
+    private val screenInteractor = ScreenInteractor(screenWidth, screenHeight, spriteWidth, spriteHeight)
+
+    fun movePlayerRight() {
+        playerGoingRight = true
+        val newPlayerLocation = Pair(playerLocation.first + movementIncrement, playerLocation.second)
+        if(screenInteractor.isInBounds(newPlayerLocation)) {
+            playerLocation = newPlayerLocation
+        }
+
+    }
+
+    fun movePlayerLeft() {
+        playerGoingRight = false
+        val newPlayerLocation = Pair(playerLocation.first - movementIncrement, playerLocation.second)
+        if(screenInteractor.isInBounds(newPlayerLocation)) {
+            playerLocation = newPlayerLocation
+        }
+    }
+
+
+}
