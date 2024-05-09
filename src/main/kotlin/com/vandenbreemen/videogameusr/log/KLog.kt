8d947ef641650b5f6b6951cf20b0be24f7b63ce0
Kevin <kevin.vandenbreemen@gmail.com>
2024-05-09 09:51:36 -0700

    Logging - add a queue and do printouts in a separate thread

--- src/main/kotlin/com/vandenbreemen/videogameusr/log/KLog.kt
+++ src/main/kotlin/com/vandenbreemen/videogameusr/log/KLog.kt
@@ -1,5 +1,12 @@
 package com.vandenbreemen.com.vandenbreemen.videogameusr.log
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
+
 /*
 ------------------~~~~~~~~~~~~~~~~----------------------------
 Simple logging library that will eventually have pluggability
@@ -10,16 +17,62 @@ can see what's going on
 
 
 enum class KlogLevel {
+    DEBUG,
     INFO,
     WARN,
     ERROR
+}
 
+private val dispatcher = Dispatchers.Default
+private const val LOG_DELAY = 100L
+private val dateFormat = SimpleDateFormat("HH:mm:ss.SSS")
+private data class Entry (
+    val timestamp: Long,
+    val level: KlogLevel,
+    val message: String,
+    val throwable: Throwable? = null
+
+) {
+    fun toLogString(): String {
+
+        val stackTrace = throwable?.stackTrace?.joinToString("\n") ?: ""
+
+        val formatted = dateFormat.format(timestamp)
+        return "$formatted: $level: $message${if(stackTrace.isNotBlank()) "\n$stackTrace" else ""}"
+    }
+}
+
+private val loqQueue = Vector<Entry>()
+val logJob = CoroutineScope(dispatcher).launch {
+
+    //  Hours, minutes, seconds, milliseconds
+    while (true) {
+        delay(LOG_DELAY)
+        if (loqQueue.isNotEmpty()) {
+            loqQueue.removeAll { entry ->
+                println(entry.toLogString())
+                true
+            }
+        }
+    }
+}
+
+private fun logOutOfBand(timestamp: Long, level: KlogLevel, message: String, throwable: Throwable? = null) {
+    loqQueue.add(Entry(timestamp, level, message, throwable))
 }
 
 fun klog(level: KlogLevel, message: String) {
-    println("KLOG: $level: $message")
+    logOutOfBand(System.currentTimeMillis(), level, message)
+}
+
+fun klog(level: KlogLevel, message: String, throwable: Throwable) {
+    logOutOfBand(System.currentTimeMillis(), level, message, throwable)
 }
 
 fun klog(message: String) {
     klog(KlogLevel.INFO, message)
+}
+
+fun klog(message: String, throwable: Throwable) {
+    klog(KlogLevel.INFO, message, throwable)
 }
