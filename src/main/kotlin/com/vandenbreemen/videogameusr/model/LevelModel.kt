6ec0fb8dcb9d5a6f18bbf759d98a872d7ca99f43
Kevin <kevin.vandenbreemen@gmail.com>
2024-05-10 10:05:18 -0700

    Start defining "levels"

---
+++ src/main/kotlin/com/vandenbreemen/videogameusr/model/LevelModel.kt
@@ -0,0 +1,33 @@
+package com.vandenbreemen.videogameusr.model
+
+import com.vandenbreemen.viddisplayrast.data.GameDataRequirements
+
+/**
+ * Description of a "level" in the game
+ */
+class LevelModel(private val requirements: GameDataRequirements, val widthInTiles: Int, val heightInTiles: Int) {
+
+    //  2-d array of sprite indexes
+    private val levelData = Array(widthInTiles) { IntArray(heightInTiles) }
+
+    /**
+     * Specify sprite to go at the given tile
+     */
+    fun setSpriteTileAt(x: Int, y: Int, spriteIndex: Int){
+        //  Verify the sprite index is good
+        val maxSprites = requirements.maxBytes / (requirements.spriteWidth * requirements.spriteHeight)
+        if(spriteIndex !in 0 until maxSprites){
+            throw IllegalArgumentException("Invalid sprite index $spriteIndex, max sprites=$maxSprites")
+        }
+
+        levelData[x][y] = spriteIndex
+    }
+
+    /**
+     * Get the sprite index at the given tile
+     */
+    fun getSpriteTileAt(x: Int, y: Int): Int {
+        return levelData[x][y]
+    }
+
+}
---
+++ src/test/kotlin/com/vandenbreemen/videogameusr/model/LevelModelTest.kt
@@ -0,0 +1,45 @@
+package com.vandenbreemen.videogameusr.model
+
+import com.vandenbreemen.viddisplayrast.data.GameDataRequirements
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+
+class LevelModelTest {
+
+    val requirements = GameDataRequirements(100, 100, 8, 8, 1024)
+
+    @Test
+    fun `should register sprite index`() {
+
+        val levelModel = LevelModel(requirements, 1000, 100)
+
+        levelModel.setSpriteTileAt(10, 19, 5)
+        assertEquals(5, levelModel.getSpriteTileAt(10, 19))
+
+
+    }
+
+    @Test
+    fun `should prevent adding sprite of index out of bounds`() {
+
+        val levelModel = LevelModel(requirements, 1000, 100)
+
+        assertThrows(IllegalArgumentException::class.java) {
+            levelModel.setSpriteTileAt(10, 19, 16)
+        }
+    }
+
+    @Test
+    fun `should prevent adding sprite of index out of bounds neg index`() {
+
+        val levelModel = LevelModel(requirements, 1000, 100)
+
+        assertThrows(IllegalArgumentException::class.java) {
+            levelModel.setSpriteTileAt(10, 19, -1)
+        }
+    }
+
+
+
+}
