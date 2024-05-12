4c5f6fe14f5c626633a7c0def39c0313297fe8eb
Kevin <kevin.vandenbreemen@gmail.com>
2024-05-11 21:12:19 -0700

    Don't allow going beyond the edges of the level

--- src/main/kotlin/com/vandenbreemen/videogameusr/model/LevelRenderingInteractor.kt
+++ src/main/kotlin/com/vandenbreemen/videogameusr/model/LevelRenderingInteractor.kt
@@ -12,18 +12,34 @@ class LevelRenderingInteractor(
     private val cameraWidthHeight = Pair(requirements.screenWidth, requirements.screenHeight)
 
     fun moveCameraRight(){
+        //  Ensure camera width doesn't go beyond level width
+        if(cameraLocation.first + cameraWidthHeight.first / requirements.spriteWidth >= levelModel.widthInTiles){
+            return
+        }
         cameraLocation = Pair(cameraLocation.first + 1, cameraLocation.second)
     }
 
     fun moveCameraLeft(){
+        //  Ensure camera width doesn't go beyond level width
+        if(cameraLocation.first == 0){
+            return
+        }
         cameraLocation = Pair(cameraLocation.first - 1, cameraLocation.second)
     }
 
     fun moveCameraUp(){
+        //  Ensure camera height doesn't go beyond level height
+        if(cameraLocation.second == 0){
+            return
+        }
         cameraLocation = Pair(cameraLocation.first, cameraLocation.second - 1)
     }
 
     fun moveCameraDown(){
+        //  Ensure camera height doesn't go beyond level height
+        if(cameraLocation.second + cameraWidthHeight.second / requirements.spriteHeight >= levelModel.heightInTiles){
+            return
+        }
         cameraLocation = Pair(cameraLocation.first, cameraLocation.second + 1)
     }
 
