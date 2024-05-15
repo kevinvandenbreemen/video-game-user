94f070cebc4bf3d97e9fae42238c8bc6c42f4cf0
Kevin <kevin.vandenbreemen@gmail.com>
2024-05-15 16:51:34 -0700

    Pan camera around inside the view

--- src/main/kotlin/com/vandenbreemen/videogameusr/view/render/RunnerView.kt
+++ src/main/kotlin/com/vandenbreemen/videogameusr/view/render/RunnerView.kt
@@ -25,4 +25,58 @@ class RunnerView(private val requirements: GameDataRequirements): Runner(require
             cameraViewEnd.second
         )
     }
+
+    /**
+     * Move the camera left
+     * @return True if the camera was moved, false if it was already at the edge
+     */
+    fun moveCameraLeft(): Boolean {
+        if(cameraViewStart.first == 0){
+            return false
+        }
+        cameraViewStart = Pair(cameraViewStart.first - 1, cameraViewStart.second)
+        cameraViewEnd = Pair(cameraViewEnd.first - 1, cameraViewEnd.second)
+        return true
+    }
+
+    /**
+     * Move the camera right
+     * @return True if the camera was moved, false if it was already at the edge
+     */
+    fun moveCameraRight(): Boolean {
+        if(cameraViewEnd.first == requirements.screenWidth - 1){
+            return false
+        }
+        cameraViewStart = Pair(cameraViewStart.first + 1, cameraViewStart.second)
+        cameraViewEnd = Pair(cameraViewEnd.first + 1, cameraViewEnd.second)
+        return true
+    }
+
+    /**
+     * Move the camera up
+     * @return True if the camera was moved, false if it was already at the edge
+     */
+    fun moveCameraUp(): Boolean {
+        if(cameraViewStart.second == 0){
+            return false
+        }
+        cameraViewStart = Pair(cameraViewStart.first, cameraViewStart.second - 1)
+        cameraViewEnd = Pair(cameraViewEnd.first, cameraViewEnd.second - 1)
+        return true
+    }
+
+    /**
+     * Move the camera down
+     * @return True if the camera was moved, false if it was already at the edge
+     */
+    fun moveCameraDown(): Boolean {
+        if(cameraViewEnd.second == requirements.screenHeight - 1){
+            return false
+        }
+        cameraViewStart = Pair(cameraViewStart.first, cameraViewStart.second + 1)
+        cameraViewEnd = Pair(cameraViewEnd.first, cameraViewEnd.second + 1)
+        return true
+    }
+
+
 }
