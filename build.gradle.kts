8b6cff3fc0dab28e7fd8a37175489801af44ad3f
Kevin <kevin.vandenbreemen@gmail.com>
2024-05-14 21:03:10 -0700

    Update display raster package version

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
 
-    val videoDisplayRasterVersion = "1.0.2.7"
+    val videoDisplayRasterVersion = "1.0.2.8"
     implementation("com.vandenbreemen:video-display-raster:$videoDisplayRasterVersion")
 
     implementation(compose.runtime)
