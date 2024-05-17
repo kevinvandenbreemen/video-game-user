ae7a571571a43f2f48064e094367c83c10f03b75
Kevin <kevin.vandenbreemen@gmail.com>
2024-05-16 17:04:34 -0700

    Game 4 - start work

---
+++ src/main/kotlin/com/vandenbreemen/videogameusr/game/vidgame4/VideoGame4.kt
@@ -0,0 +1,8 @@
+package com.vandenbreemen.videogameusr.game.vidgame4
+
+fun main() {
+
+    val model = VideoGame4Model()
+    model.edit()
+
+}
---
+++ src/main/kotlin/com/vandenbreemen/videogameusr/game/vidgame4/VideoGame4Model.kt
@@ -0,0 +1,22 @@
+package com.vandenbreemen.videogameusr.game.vidgame4
+
+import com.vandenbreemen.viddisplayrast.data.GameDataRequirements
+import com.vandenbreemen.videogameusr.model.game.TileBasedGameWorld
+import com.vandenbreemen.videogameusr.tools.gameEditor
+
+class VideoGame4Model {
+
+    //  100 possible sprite tiles
+    //  SNES style
+    private val requirements = GameDataRequirements(256, 224, 8, 8, 6400 )
+
+    private val world = TileBasedGameWorld(requirements)
+
+    /**
+     * Triggers the game editor for the game.  Use only for development purposes
+     */
+    fun edit() {
+        gameEditor(requirements, world, 0, "requirements")
+    }
+
+}
