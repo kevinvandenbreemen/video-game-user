8f8b069b716aae96133abf58f4cb22d0dcdd22dc
Kevin <kevin.vandenbreemen@gmail.com>
2024-05-19 10:08:53 -0700

    Make this more presentable

--- src/main/kotlin/com/vandenbreemen/videogameusr/tools/composables/LevelEditorUI.kt
+++ src/main/kotlin/com/vandenbreemen/videogameusr/tools/composables/LevelEditorUI.kt
@@ -6,8 +6,12 @@ import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
@@ -15,6 +19,7 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.IntSize
 import com.vandenbreemen.com.vandenbreemen.videogameusr.log.klog
+import com.vandenbreemen.com.vandenbreemen.videogameusr.view.Dimensions
 import com.vandenbreemen.com.vandenbreemen.videogameusr.view.VideoGameUserTheme
 import com.vandenbreemen.viddisplayrast.data.GameDataRequirements
 import com.vandenbreemen.videogameusr.model.CoreDependenciesHelper
@@ -40,13 +45,17 @@ fun LevelDesigner(levelEditorViewModel: LevelEditorViewModel) {
             levelEditorViewModel.selectSpriteIndex(selectedSpriteIndex.value)
         }
 
-        Column(modifier = Modifier.weight(.1f)) {
-            //  The tile selector
-            SpriteTileGrid(levelEditorViewModel.getSpriteEditorModel(),  "All Tile Assets", selectedSpriteIndex::value::set)
+        Column(modifier = Modifier.weight(.12f)) {
+            Card(modifier = Modifier.padding(Dimensions.padding).fillMaxSize(), elevation = Dimensions.elevation) {
+                //  The tile selector
+                SpriteTileGrid(levelEditorViewModel.getSpriteEditorModel(), "Tiles", selectedSpriteIndex::value::set)
+            }
         }
-        Column(modifier = Modifier.weight(.9f)) {
+        Column(modifier = Modifier.weight(.88f).clip(MaterialTheme.shapes.medium).fillMaxSize()) {
             //  The level editor
-            LevelEditorView(levelEditorViewModel)
+            Card(modifier = Modifier.padding(Dimensions.padding).fillMaxSize(), elevation = Dimensions.elevation){
+                LevelEditorView(levelEditorViewModel)
+            }
         }
     }
 }
@@ -93,83 +102,81 @@ fun LevelEditorView(levelEditorViewModel: LevelEditorViewModel) {
     val selectedSpriteIndex by levelEditorViewModel.currentSelectedSpriteIndexStateFlow.collectAsState()
     val tileIndexGridForLevel = levelEditorViewModel.levelCoordinateToTileGrid.collectAsState()
 
-    Column(modifier=Modifier.fillMaxSize(), horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally) {
 
-        //  Show a grid of squares corresponding to the zoom etc
-        Canvas(modifier = Modifier.fillMaxSize().pointerInput(Unit) {
+    //  Show a grid of squares corresponding to the zoom etc
+    Canvas(modifier = Modifier.fillMaxSize().pointerInput(Unit) {
 
-            //  Detect user tapping on a tile and set its sprite
-            detectTapGestures {
-
-                val gridRenderinInfo = getLevelGridRenderingInfo(size, levelEditorViewModel)
-
-                val x = it.x
-                val y = it.y
-
-                val casnvasWidth = size.width
-                val canvasHeight = size.height
-
-
-
-                val boxWidth = gridRenderinInfo.boxWidth
-                val boxHeight = gridRenderinInfo.boxHeight
-                val col = (x / boxWidth).toInt()
-                val row = (y / boxHeight).toInt()
-                klog("tapped ($x, $y), Canvas width, height=($casnvasWidth, $canvasHeight), Level Width, Height = (${levelEditorViewModel.levelWidth}, ${levelEditorViewModel.levelHeight}), Box width,height =($boxWidth, $boxHeight) --> Tap on row=$row, col=$col")
-
-                val index = selectedSpriteIndex
-                levelEditorViewModel.setSpriteTileAt(col, row, index)
-            }
-
-        }
-        ) {
+        //  Detect user tapping on a tile and set its sprite
+        detectTapGestures {
 
             val gridRenderinInfo = getLevelGridRenderingInfo(size, levelEditorViewModel)
 
-            val spriteWidth = levelEditorViewModel.spriteWidth.toFloat()
-            val spriteHeight = levelEditorViewModel.spriteHeight.toFloat()
+            val x = it.x
+            val y = it.y
 
             val casnvasWidth = size.width
             val canvasHeight = size.height
 
+
+
             val boxWidth = gridRenderinInfo.boxWidth
             val boxHeight = gridRenderinInfo.boxHeight
+            val col = (x / boxWidth).toInt()
+            val row = (y / boxHeight).toInt()
+            klog("tapped ($x, $y), Canvas width, height=($casnvasWidth, $canvasHeight), Level Width, Height = (${levelEditorViewModel.levelWidth}, ${levelEditorViewModel.levelHeight}), Box width,height =($boxWidth, $boxHeight) --> Tap on row=$row, col=$col")
 
-            val pixelWidth = gridRenderinInfo.pixelWidth
-            val pixelHeight = gridRenderinInfo.pixelHeight
+            val index = selectedSpriteIndex
+            levelEditorViewModel.setSpriteTileAt(col, row, index)
+        }
+
+    }
+    ) {
+
+        val gridRenderinInfo = getLevelGridRenderingInfo(size, levelEditorViewModel)
+
+        val spriteWidth = levelEditorViewModel.spriteWidth.toFloat()
+        val spriteHeight = levelEditorViewModel.spriteHeight.toFloat()
+
+        val casnvasWidth = size.width
+        val canvasHeight = size.height
+
+        val boxWidth = gridRenderinInfo.boxWidth
+        val boxHeight = gridRenderinInfo.boxHeight
+
+        val pixelWidth = gridRenderinInfo.pixelWidth
+        val pixelHeight = gridRenderinInfo.pixelHeight
 
 
 
-            klog("canvas width=$casnvasWidth, Box width=$boxWidth, box height=$boxHeight")
+        klog("canvas width=$casnvasWidth, Box width=$boxWidth, box height=$boxHeight")
 
-            //  Draw the rows as boxes
-            for (row in 0 until levelEditorViewModel.levelHeight) {
-                for (col in 0 until levelEditorViewModel.levelWidth) {
-                    drawRect(
-                        color = Color.Black,
-                        topLeft = Offset(col * boxWidth, row * boxHeight),
-                        size = Size(boxWidth, boxHeight),
-                        style = Stroke(0.5f)
-                    )
+        //  Draw the rows as boxes
+        for (row in 0 until levelEditorViewModel.levelHeight) {
+            for (col in 0 until levelEditorViewModel.levelWidth) {
+                drawRect(
+                    color = Color.Black,
+                    topLeft = Offset(col * boxWidth, row * boxHeight),
+                    size = Size(boxWidth, boxHeight),
+                    style = Stroke(0.1f)
+                )
 
-                    //  Now grab the sprite from the level and draw it
-                    val index = tileIndexGridForLevel.value[row][col]
-                    val spriteColorGrid = levelEditorViewModel.getSpritePixelColorGridForSpriteIndex(index)
-                    if (spriteColorGrid != null) {
-                        for (y in 0 until spriteHeight.toInt()) {
-                            for (x in 0 until spriteWidth.toInt()) {
-                                drawRect(
-                                    color = spriteColorGrid[y][x],
-                                    topLeft = Offset((col * boxWidth) + (x*pixelWidth), (row * boxHeight) + (y*pixelHeight)),
-                                    size = Size(pixelWidth, pixelHeight)
-                                )
-                            }
+                //  Now grab the sprite from the level and draw it
+                val index = tileIndexGridForLevel.value[row][col]
+                val spriteColorGrid = levelEditorViewModel.getSpritePixelColorGridForSpriteIndex(index)
+                if (spriteColorGrid != null) {
+                    for (y in 0 until spriteHeight.toInt()) {
+                        for (x in 0 until spriteWidth.toInt()) {
+                            drawRect(
+                                color = spriteColorGrid[y][x],
+                                topLeft = Offset((col * boxWidth) + (x*pixelWidth), (row * boxHeight) + (y*pixelHeight)),
+                                size = Size(pixelWidth, pixelHeight)
+                            )
                         }
                     }
                 }
             }
-
         }
+
     }
 
 }
