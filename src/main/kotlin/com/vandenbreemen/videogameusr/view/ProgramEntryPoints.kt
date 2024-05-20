a8ea607c8da6c7e0fcdbbb871351e2d3bfd2d8cd
Kevin <kevin.vandenbreemen@gmail.com>
2024-05-20 10:37:14 -0700

    Code cleanup

--- src/main/kotlin/com/vandenbreemen/videogameusr/view/ProgramEntryPoints.kt
+++ src/main/kotlin/com/vandenbreemen/videogameusr/view/ProgramEntryPoints.kt
@@ -1,5 +1,8 @@
 package com.vandenbreemen.com.vandenbreemen.videogameusr.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowState
@@ -7,27 +10,11 @@ import androidx.compose.ui.window.application
 import com.vandenbreemen.com.vandenbreemen.videogameusr.controller.VideoGameController
 import com.vandenbreemen.com.vandenbreemen.videogameusr.log.KlogLevel
 import com.vandenbreemen.com.vandenbreemen.videogameusr.log.klog
-import com.vandenbreemen.viddisplayrast.data.DisplayRaster
 
 
 class ProgramEntryPoints {
 
     companion object {
-        fun showTestRenderWindow(raster: DisplayRaster, maxWidth: Int = 800) = application {
-
-            //  Step 1:  Work out the height as a ratio of the width
-            val ratio  = raster.yDim.toFloat() / raster.xDim.toFloat()
-            val height = (maxWidth * ratio).toInt()
-
-            Window(
-                onCloseRequest = {  },
-                visible = true,
-                title = "Raster Render Test",
-                state = WindowState(width = maxWidth.dp, height = height.dp)
-            ) {
-                RasterDisplay(raster, DummyVideoGameController())
-            }
-        }
 
         fun playGameInWindow(controller: VideoGameController, framesPerSecond: Int = 60, maxWidth: Int = 800) = application {
 
