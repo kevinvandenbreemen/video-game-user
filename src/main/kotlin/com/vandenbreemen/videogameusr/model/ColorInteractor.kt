7d3381f169136da5b7fa977484df08d3c68b57ff
Kevin <kevin.vandenbreemen@gmail.com>
2024-05-22 18:16:24 -0700

    Cache - colors

--- src/main/kotlin/com/vandenbreemen/videogameusr/model/ColorInteractor.kt
+++ src/main/kotlin/com/vandenbreemen/videogameusr/model/ColorInteractor.kt
@@ -5,10 +5,13 @@ import com.vandenbreemen.viddisplayrast.data.ByteColorDataInteractor
 
 class ColorInteractor(private val byteColorDataInteractor: ByteColorDataInteractor = ByteColorDataInteractor()) {
 
+    private val colorMap = mutableMapOf<Byte, Color>()
+
     fun getComposeColor(colorByte: Byte): Color {
+        colorMap[colorByte]?.let { return it }
 
         if(colorByte.toInt() == 0) {
-            return Color(0, 0, 0, 0,)
+            return Color(0, 0, 0, 0,).also { colorMap[colorByte] = it}
         }
 
         val redRaw = byteColorDataInteractor.getRed(colorByte)
@@ -19,7 +22,7 @@ class ColorInteractor(private val byteColorDataInteractor: ByteColorDataInteract
         val green = greenRaw.toFloat() / 255
         val blue = blueRaw.toFloat() / 255
 
-        return Color(red, green, blue)
+        return Color(red, green, blue).also { colorMap[colorByte] = it}
     }
 
 }
