c6592440584b9f13a1e43fc2d66b39884c930b68
Kevin <kevin.vandenbreemen@gmail.com>
2024-05-23 21:42:49 -0700

    Make confirming button simpler

--- src/main/kotlin/com/vandenbreemen/videogameusr/view/common/ConfirmingButton.kt
+++ src/main/kotlin/com/vandenbreemen/videogameusr/view/common/ConfirmingButton.kt
@@ -2,21 +2,19 @@ package com.vandenbreemen.videogameusr.view.common
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.vandenbreemen.videogameusr.view.Dimensions
 import com.vandenbreemen.videogameusr.view.VideoGameUserTheme
 
 @Composable
@@ -29,36 +27,44 @@ fun ConfirmingButton(
 
     val isConfirming = remember { mutableStateOf(false) }
 
-    Row {
-        if(!isConfirming.value) {
-            Button(onClick = { isConfirming.value = true }) {
-                Text(text, style = MaterialTheme.typography.button)
+    Card(elevation= Dimensions.elevation, shape = MaterialTheme.shapes.medium,
+        modifier = Modifier.padding(horizontal = Dimensions.borderPadding)) {
+        Row(modifier = Modifier.padding(Dimensions.borderPadding)) {
+            if(!isConfirming.value) {
+                Button(
+                    onClick = {
+                        isConfirming.value = true
+                    }, modifier=Modifier.height((Dimensions.padding.value*2.5).dp)
+                ) {
+                    Text(text, style = MaterialTheme.typography.button)
+                }
             }
-        }
-        else {
-            Column(modifier=Modifier.background(Color.Yellow)) {
-                Text(areYouSureMsg, style = MaterialTheme.typography.button)
-                Row {
-                    Button(
-                        onClick = {
-                            isConfirming.value = false
-                            onConfirm()
+            else {
+                Column(modifier=Modifier.background(MaterialTheme.colors.secondary).padding(horizontal = Dimensions.padding, vertical = 2.dp)) {
+                    Text(areYouSureMsg, style = MaterialTheme.typography.button)
+                    Row() {
+                        Button(
+                            onClick = {
+                                isConfirming.value = false
+                                onCancel()
+                            }, modifier=Modifier.width((Dimensions.padding.value * 4.5).dp).height((Dimensions.padding.value*2.5).dp)
+                        ) {
+                            Text("No", style = MaterialTheme.typography.button)
                         }
-                    ) {
-                        Text("Yes", style = MaterialTheme.typography.button)
-                    }
-                    Spacer(modifier = Modifier.width(10.dp))
-                    Button(
-                        onClick = {
-                            isConfirming.value = false
-                            onCancel()
+                        Spacer(modifier = Modifier.width(10.dp))
+                        Button(
+                            onClick = {
+                                isConfirming.value = false
+                                onConfirm()
+                            }, modifier=Modifier.width((Dimensions.padding.value * 5).dp).height((Dimensions.padding.value*2.5).dp)
+                        ) {
+                            Text("Yes", style = MaterialTheme.typography.button)
                         }
-                    ) {
-                        Text("No", style = MaterialTheme.typography.caption)
                     }
                 }
             }
         }
+
     }
 }
 
