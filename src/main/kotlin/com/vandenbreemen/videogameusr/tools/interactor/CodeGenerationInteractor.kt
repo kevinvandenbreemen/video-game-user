daf151f228c053da47037465652fa4fe16cb5bb5
Kevin <kevin.vandenbreemen@gmail.com>
2024-05-29 19:55:54 -0700

    Get rid of un-used code

--- src/main/kotlin/com/vandenbreemen/videogameusr/tools/interactor/CodeGenerationInteractor.kt
+++ src/main/kotlin/com/vandenbreemen/videogameusr/tools/interactor/CodeGenerationInteractor.kt
@@ -10,57 +10,11 @@ import java.io.File
 
 class CodeGenerationInteractor(private val requirements: GameDataRequirements) {
 
-    private var fileToWriteTo = "generated/SpriteData.kt"
     private val tileDataFileToWrite = "generated/TileData.kt"
     private val codeGenerationModel = CoreDependenciesHelper.getCodeGenerationModel()
 
-    fun generateCodeForSpriteIndex(spriteIndex: Int, requirementsVariableName: String): String {
-
-        val byteArrayToCode = getSpriteTileGridArray(spriteIndex)
-
-        val stringBld = StringBuilder("""
-$requirementsVariableName.setData($spriteIndex, byteArrayOf(""")
-        byteArrayToCode.forEachIndexed { index, byte ->
-            if(index % requirements.spriteWidth == 0){
-                stringBld.append("\n       ")
-            }
-            stringBld.append(byte)
-            if(index < byteArrayToCode.size - 1){
-                stringBld.append(", ")
-            }
-        }
-        stringBld.append("\n))")
-
-        return stringBld.toString()
-    }
-
     fun getSpriteTileGridArray(index: Int): ByteArray {
         return requirements.getSpriteData(index)
-    }
-
-    /**
-     * Write out all sprites to a file
-     */
-    fun writeAllSpritesToFile() {
-
-        CoroutineScope(CoreDependenciesHelper.getIODispatcher()).launch {
-
-            codeGenerationModel.setupCodeGeneratingDirectory()
-
-            val codeBuilder = StringBuilder()
-            generateSpriteSheet(codeBuilder)
-
-            File(fileToWriteTo).writeText(codeBuilder.toString())
-            klog("Wrote all sprites to $fileToWriteTo")
-        }
-
-    }
-
-    private fun generateSpriteSheet(codeBuilder: StringBuilder) {
-        for (index in 0 until (requirements.maxBytes / (requirements.spriteWidth * requirements.spriteHeight))) {
-            codeBuilder.append(generateCodeForSpriteIndex(index, "requirements"))
-            codeBuilder.append("\n\n")
-        }
     }
 
     fun writeLevelToFile(levelModel: LevelModel) {
