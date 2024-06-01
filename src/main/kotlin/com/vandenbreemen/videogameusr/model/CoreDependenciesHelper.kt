f866f5301ccea1990864b55060422e1b08cd2be2
Kevin <kevin.vandenbreemen@gmail.com>
2024-06-01 09:20:27 -0700

    Make creating SNES based screen requirements easier

--- src/main/kotlin/com/vandenbreemen/videogameusr/model/CoreDependenciesHelper.kt
+++ src/main/kotlin/com/vandenbreemen/videogameusr/model/CoreDependenciesHelper.kt
@@ -1,6 +1,7 @@
 package com.vandenbreemen.videogameusr.model
 
 import com.vandenbreemen.viddisplayrast.data.ByteColorDataInteractor
+import com.vandenbreemen.viddisplayrast.data.GameDataRequirements
 import com.vandenbreemen.videogameusr.view.render.CanvasRasterRender
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
@@ -21,6 +22,18 @@ object CoreDependenciesHelper {
 
     fun getCanvasRasterRender(): CanvasRasterRender {
         return CanvasRasterRender(getColorInteractor())
+    }
+
+    /**
+     * Provide for master game data requirements for the entire game!
+     * See also https://snes.nesdev.org/wiki/Tiles
+     */
+    fun createRequirements(width: Int, height: Int, tileWidth: Int, tileHeight: Int, numTiles: Int): GameDataRequirements {
+        return GameDataRequirements(width, height, tileWidth, tileHeight, numTiles * (tileWidth * tileHeight))
+    }
+
+    fun createSNESRequirements(numTiles: Int): GameDataRequirements {
+        return createRequirements(256, 224, 8, 8, numTiles)
     }
 
 }
